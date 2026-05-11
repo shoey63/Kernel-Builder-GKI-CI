6c7bd976ef50c92e5b40279d02032f3b1fdcbd9e
shoey63 <shoey63@users.noreply.github.com>
2026-05-11 15:33:26 +0800

    Refactor kernel build script to use official hash
    Removed git commit steps and updated build process to use official hash.

--- scripts/build_kernel.sh
+++ scripts/build_kernel.sh
@@ -7,12 +7,6 @@ mkdir -p ../out out/dist
 [ -d common ] || { echo "[-] common/ not found in kernel_workspace" >&2; exit 1; }
 [ -x tools/bazel ] || { echo "[-] tools/bazel not found or not executable" >&2; exit 1; }
 
-# Capture the PURE Google head 
-GKI_HASH=$(git rev-parse --short HEAD)
-GKI_DATE=$(git log -1 --format=%ct)
-
-echo ">>> Latest commit Locked: g$GKI_HASH (Timestamp: $GKI_DATE)"
-
 echo ">>> Neutralizing ABI protected exports lists..."
 for f in common/android/abi_gki_protected_exports*; do
   if [ -f "$f" ]; then
@@ -20,26 +14,14 @@ for f in common/android/abi_gki_protected_exports*; do
   fi
 done
 
-echo ">>> Satisfying Kleaf's git status checks to remove -dirty and fix timestamp..."
-cd common
-
-# Configure local git identity
-git config --global user.name "github-actions[bot]"
-git config --global user.email "github-actions[bot]@users.noreply.github.com"
-
-# Stage and commit changes
-git add .
-git commit -m "ci: integrated KSU, SUSFS, and other patches if any" || true
-cd ..
-
-echo ">>> Compiling common Android arm64 kernel..."
-
+echo ">>> Collecting Latest Hash and Commencing build: g$OFFICIAL_HASH"
 tools/bazel run --config=local --config=stamp \
-  --action_env=SOURCE_DATE_EPOCH="$GKI_DATE" \
-  --action_env=STABLE_BUILD_VERSION="g$GKI_HASH" \
-  --action_env=KLEAF_KERNEL_BUILD_VERSION="g$GKI_HASH" \
+  --action_env=SOURCE_DATE_EPOCH="$OFFICIAL_DATE" \
+  --action_env=STABLE_BUILD_VERSION="g$OFFICIAL_HASH" \
+  --action_env=KLEAF_KERNEL_BUILD_VERSION="g$OFFICIAL_HASH" \
   --action_env=KLEAF_SKIP_ABI_CHECKS=true \
   //common:kernel_aarch64_dist -- --destdir=out/dist
+
 
 IMAGE_PATH="$(find out/dist -type f -name 'Image' | head -n1)"
 
